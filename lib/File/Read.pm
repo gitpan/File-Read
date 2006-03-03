ef477f9c8d293c1aaf8c4b8d910dfb45775d63e5
Sébastien Aperghis-Tramoni <saper@cpan.org>
2006-03-03 17:29:46 +0000

    Import of SAPER/File-Read-0.01 from CPAN.
    gitpan-cpan-distribution: File-Read
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SAPER/File-Read-0.01.tar.gz
    gitpan-cpan-author:       SAPER
    gitpan-cpan-maturity:     released

---
+++ lib/File/Read.pm
@@ -0,0 +1,282 @@
+package File::Read;
+use strict;
+use Carp;
+use File::Slurp ();
+require Exporter;
+
+{   no strict;
+    $VERSION = '0.01';
+    @ISA = qw(Exporter);
+    @EXPORT = qw(read_file read_files);
+}
+
+*read_files = \&read_file;
+
+=head1 NAME
+
+File::Read - Unique interface for reading one or more files
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+    use File::Read;
+
+    # read a file
+    $file = read_file($path);
+
+    # read several files
+    @files = read_files(@paths);
+
+    # aggregate several files
+    $file = read_files(@paths);
+
+    # read a file as root, skip comments and blank lines
+    $file = read_file({ as_root => 1, skip_comments => 1, skip_blanks => 1 }, $path);
+
+
+=head1 DESCRIPTION
+
+This module mainly proposes functions for reading one or more files, 
+with different options. See below for more details and examples.
+
+
+=head1 EXPORT
+
+By default, this module exports all the functions documented afterhand.
+It also recognizes import options. For example
+
+    use File::Read 'err_mode=quiet';
+
+set C<read_file()>'s C<err_mode> option default value to C<"quiet">.
+
+=head1 FUNCTIONS
+
+=over
+
+=item B<read_file()>
+
+Read the files given in argument and return their content. 
+Options can be set using a hashref as first parameter.
+
+B<Options>
+
+=over
+
+=item *
+
+C<aggregate> controls how the functions returns the content of the files 
+that was successfully read. By default, its behaviour depends on the 
+Perl context (list or scalar). This option is used to force it to return 
+a scalar (when set to true) or a list (when set to false).
+
+=item *
+
+C<as_root> tells the function to read the given file(s) as root using 
+B<sudo(8)> and B<cat(1)>.
+
+=item *
+
+C<err_mode> controls how the function behaves when an error occurs. 
+Available values are C<"croak">, C<"carp"> and C<"quiet">.
+Default value is C<"croak">.
+
+=item *
+
+C<skip_comments> tells the functions to remove all comment lines from 
+the read files. 
+
+=item *
+
+C<skip_blanks> tells the functions to remove all blank lines from 
+the read files. 
+
+=back
+
+B<Examples>
+
+Just read a file:
+
+    my $file = read_file($path);
+
+Read a file, skipping comments:
+
+    my $file = read_file({ skip_comments => 1 }, $path);
+
+Read several files, skipping blank lines and comments:
+
+    my @files = read_file({ skip_comments => 1, skip_blanks => 1 }, @paths);
+
+=item B<read_files()>
+
+<read_files()> is just an alias for C<read_file()> so that it look more 
+sane when reading several files. 
+
+=cut
+
+my %defaults = (
+    err_mode        => 'croak', 
+    skip_comments   => 0, 
+    skip_blanks     => 0, 
+);
+
+sub import {
+    my($module,@args) = @_;
+    my @new = ();
+
+    # parse arguments
+    for my $arg (@args) {
+        if(index($arg, '=') >= 0) {
+            my($opt,$val) = split('=', $arg);
+            $defaults{$opt} = $val if exists $defaults{$opt};
+
+        } else {
+            push @new, $arg
+        }
+    }
+
+    $module->export_to_level(1, $module, @new);
+}
+
+sub read_file {
+    my %opts = ref $_[0] eq 'HASH' ? %{+shift} : ();
+    my @paths = @_;
+    my @files = ();
+
+    # check options
+    for my $opt (keys %defaults) {
+        $opts{$opt} = $defaults{$opt} unless defined $opts{$opt}
+    }
+
+    # define error handler
+    $opts{err_mode} =~ /^(?:carp|croak|quiet)$/
+        or croak "error: Bad value '$opts{err_mode}' for option 'err_mode'";
+
+    my %err_with = (
+        'carp'  => \&carp, 
+        'croak' => \&croak, 
+        'quiet' => sub{}, 
+    );
+    my $err_sub = $err_with{$opts{err_mode}};
+
+    $err_sub->("error: This function needs at least one path") unless @paths;
+
+    for my $path (@paths) {
+        my @lines = ();
+        my $error = '';
+        
+        # first, read the file
+        if ($opts{as_root}) {    # ... as root
+            my $redir = $opts{err_mode} eq 'quiet' ? '2>&1' : '';
+            @lines = split $/, `sudo cat $path $redir`;
+
+            if ($?) {
+                if (not -f $path) {
+                    $! = eval q{ use Errno ':POSIX'; ENOENT } ||  2
+                } elsif (not -r $path) {
+                    $! = eval q{ use Errno ':POSIX'; EACCES } || 13
+                } else {
+                    $! = 1024
+                }
+                ($error = "$!") =~ s/ 1024//;
+            }
+
+        } else {                # ... as a normal user
+            @lines = eval { File::Slurp::read_file($path) };
+            $error = $@;
+        }
+
+        # if there's an error
+        $error and $err_sub->("error: $error");
+
+        # if there's any content at all...
+        if (@lines) {
+            # ... then do some filtering work if asked so
+            @lines = grep { ! /^$/    } @lines  if $opts{skip_blanks};
+            @lines = grep { ! /^\s*#/ } @lines  if $opts{skip_comments};
+        }
+
+        push @files, join '', @lines;
+    }
+
+    # how to return the content(s)?
+    $opts{aggregate} = !wantarray unless exists $opts{aggregate};
+
+    if($opts{aggregate}) {
+        return join '', @files 
+    } else {
+        return @files
+    }
+}
+
+=back
+
+=head1 DIAGNOSTICS
+
+=over
+
+=item Bad value '%s' for option '%s'
+
+B<(E)> You gave a bad value for the indicated option. Please check the 
+documentation for the valid values. 
+
+=item This function needs at least one path
+
+B<(E)> You called a function without giving it argument.
+
+=back
+
+=head1 AUTHOR
+
+Sébastien Aperghis-Tramoni, C<< <sebastien at aperghis.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-file-read at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-Read>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::Read
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/File-Read>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/File-Read>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-Read>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/File-Read>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 Sébastien Aperghis-Tramoni, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of File::Read
---
+++ t/file-read.t
@@ -0,0 +1,176 @@
+use strict;
+eval 'use warnings';
+use File::Read;
+use File::Spec;
+use Test::More;
+
+# describe the tests
+my @one_result_tests = (
+    {
+        args     => [ File::Spec->catfile(qw(t samples empty)) ], 
+        expected => '', 
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples space)) ], 
+        expected => ' ', 
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples newline)) ], 
+        expected => $/, 
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples pi)) ], 
+        expected => "3.14159265358979\n", 
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples hello)) ], 
+        expected => 'Hello', 
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples world)) ], 
+        expected => "world\n",
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples hello)), 
+                      File::Spec->catfile(qw(t samples space)), 
+                      File::Spec->catfile(qw(t samples world)), 
+                    ], 
+        expected => "Hello world\n", 
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples jerkcity2630)) ], 
+        expected => "DEUCE: PLEASE DO THESE STEPS IN THE FOLLOWING ORDERS:\n" .
+                    "DEUCE: 1. SHUT UP\n" .
+                    "DEUCE: 2. GET THE FUCK OUT\n",
+    }, 
+    {
+        args     => [ File::Spec->catfile(qw(t samples config)) ], 
+        expected => "# something that looks like a configuration file\n" .
+                    "# with a few comments, and some empty lines\n" .
+                    "\n" .
+                    "# enable debug\n" .
+                    "debug = 1\n" .
+                    "\n" .
+                    "# be verbose\n" .
+                    "verbose = 1\n",
+    }, 
+    {
+        args     => [ { skip_comments => 1 }, File::Spec->catfile(qw(t samples config)) ], 
+        expected => "\n" .
+                    "debug = 1\n" .
+                    "\n" .
+                    "verbose = 1\n",
+    }, 
+    {
+        args     => [ { skip_blanks => 1 }, File::Spec->catfile(qw(t samples config)) ], 
+        expected => "# something that looks like a configuration file\n" .
+                    "# with a few comments, and some empty lines\n" .
+                    "# enable debug\n" .
+                    "debug = 1\n" .
+                    "# be verbose\n" .
+                    "verbose = 1\n",
+    }, 
+    {
+        args     => [ { skip_comments => 1, skip_blanks => 1 }, 
+                      File::Spec->catfile(qw(t samples config)) ], 
+        expected => "debug = 1\n" .
+                    "verbose = 1\n",
+    }, 
+);
+
+my @one_result_root_tests = (
+    {
+        args     => [ { as_root => 1 }, 
+                      File::Spec->catfile(File::Spec->rootdir, qw(etc shadow)) ], 
+    }, 
+);
+
+my @many_results_tests = (
+    {
+        args     => [ File::Spec->catfile(qw(t samples empty)), 
+                      File::Spec->catfile(qw(t samples newline)), 
+                      File::Spec->catfile(qw(t samples space)) ], 
+        expected => [ '', $/, ' ' ], 
+    }, 
+);
+
+
+# "I love it when a plan comes together"
+plan tests => 5 + @one_result_tests * 2 + @one_result_root_tests * 2 + @many_results_tests * 3;
+
+
+# check that the advertised functions are present
+can_ok( 'File::Read' => qw(read_file read_files) );
+
+
+# check diagnostics
+eval { read_file() };
+like( $@, '/^error: This function needs at least one path/', 
+    "calling read_file() with no argument" );
+
+eval { read_files() };
+like( $@, '/^error: This function needs at least one path/', 
+    "calling read_files() with no argument" );
+
+eval { read_file('not/such/file') };
+like( $@, q{/^error: read_file 'not/such/file' - /}, 
+    "calling read_file() with a file that does not exist" );
+
+eval { read_files({ err_mode => 'pwadak' }) };
+like( $@, q{/^error: Bad value 'pwadak' for option 'err_mode'/}, 
+    "calling read_files() with invalid value for option 'err_mode'" );
+
+
+# having Data::Dumper might be useful
+eval 'use Data::Dumper';
+$Data::Dumper::Indent = 0;
+$Data::Dumper::Indent = 0;
+
+# read files, returning one result
+for my $test (@one_result_tests) {
+    (eval { Dumper($test->{args}) } || '') =~ /\[(.+)\];$/;
+    my $args_str = $1 || "@{$test->{args}}";
+
+    my $file = eval { read_file( @{$test->{args}} ) };
+    is( $@, '', "calling read_file() with args: $args_str" );
+    is( $file, $test->{expected}, "checking result" );
+}
+;
+# read files as root, returning one result
+for my $test (@one_result_root_tests) {
+    (eval { Dumper($test->{args}) } || '') =~ /\[(.+)\];$/;
+    my $args_str = $1 || "@{$test->{args}}";
+
+    # I don't know why, but Perl 5.005_04 chokes on the "SKIP:" line 
+    # with the error "Not a HASH reference". Fortunately, it's trappable 
+    # so we just eval{} it.
+    # Perl 5.8 also produces a warning "Pseudo-hashes are deprecated" 
+    # on the same line.
+    eval {
+        SKIP: {
+            skip "sudo (usually needs interactive password)", 2 
+                if exists $test->{args}{as_root} and $ENV{AUTOMATED_TESTING};
+
+            my $file = eval { read_file( @{$test->{args}} ) };
+            is( $@, '', "calling read_file() with args: $args_str" );
+            cmp_ok( length $file, '>=', 0, "checking result" );
+        }
+    };
+    if($@) {
+        # something happened, preventing the previous block to 
+        # execute, so we just skip these tests by OK'ing them
+        pass(); pass();
+    }
+}
+
+# read files, returning several results
+for my $test (@many_results_tests) {
+    (eval { Dumper($test->{args}) } || '') =~ /\[(.+)\];$/;
+    my $args_str = $1 || "@{$test->{args}}";
+
+    my @files = eval { read_file( @{$test->{args}} ) };
+    is( $@, '', "calling read_file() with args: $args_str" );
+    is( @files, scalar @{ $test->{expected} }, "checking results: number of elements" );
+    is_deeply( \@files, $test->{expected}, "checking results: deep compare" );
+}
+
